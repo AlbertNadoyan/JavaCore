b7790b3fdebe072a78069ce1337bc8abdbaa0248
Albert <albertnadoyan98@gmail.com>
2022-06-10 01:33:26 +0400

    BrachChacker homework

--- src/homeworks/brChecker/BraceChecker.java
+++ src/homeworks/brChecker/BraceChecker.java
@@ -1,49 +1,51 @@
 package homeworks.brChecker;
 
 import homeworks.stack.Stack;
-
 public class BraceChecker {
     private final String text;
    private final Stack stack = new Stack();
-
-
     BraceChecker(String text){
         this.text = text;
     }
-
-
     public void check(){
+        char chack;
         for (int i = 0; i < text.length(); i++) {
             char c = text.charAt(i);
-            if(c == '(' || c == '[' || c == '{'){
-                stack.push(c);
-            }
-
-            char check;
             switch (c){
+                case '(':
+                case '[':
+                case '{':
+                    stack.push(c);
+                    break;
                 case ')':
-                    check = (char) stack.pop();
-                    if(check == '{' || check == '['){
-                        System.out.print("opened " + check + "but close " + c + "in index " + i);
+                    chack = (char) stack.pop();
+                    if(chack == '{' || chack == '['){
+                        System.out.println("opened " + chack + "but close " + c + "in index " + i);
+                    } else if (chack != '(') {
+                        System.out.println("Error closed" + chack + "but not opened");
                     }
                     break;
                 case ']':
-                    check = (char) stack.pop();
-                    if(check == '(' || check == '{'){
-                        System.out.print("opened " + check + "but close " + c + "in index " + i);;
+                    chack = (char) stack.pop();
+                    if(chack == '(' || chack == '{'){
+                        System.out.println("opened " + chack + "but close " + c + "in index " + i);;
+                    }else if (chack != '[') {
+                        System.out.println("Error closed" + chack + "but not opened");
                     }
                     break;
                 case '}':
-                    check = (char) stack.pop();
-                    if(check == '(' || check == '['){
-                        System.out.print("opened " + check + "but close " + c + "in index " + i);
+                    chack = (char) stack.pop();
+                    if(chack == '(' || chack == '['){
+                        System.out.println("opened " + chack + "but close " + c + "in index " + i);
+                    }else if (chack != '[') {
+                        System.out.println("Error closed" + chack + "but not opened");
                     }
                     break;
             }
         }
-        char last;
-        while ((last = (char) stack.pop()) != 0) {
-            System.out.println("error opened " + last + "but not closed");
+        while ((chack = (char) stack.pop()) != 0) {
+            System.out.println("error opened " + chack + "but not closed");
         }
+
     }
 }
--- src/homeworks/brChecker/BraceCheckerDemo.java
+++ src/homeworks/brChecker/BraceCheckerDemo.java
@@ -1,12 +1,10 @@
 package homeworks.brChecker;
 
-import homeworks.stack.Stack;
-
 public class BraceCheckerDemo {
     public static void main(String[] args) {
 
-        String text = "hello { java";
-        BraceChecker braceChecker = new BraceChecker(text); //bravo))ha et el ei studel, mersi, bayc commit chem karox anel
+        String text = "))}";
+        BraceChecker braceChecker = new BraceChecker(text);
         braceChecker.check();
     }
 }
